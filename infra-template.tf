c8eacbf79f1163ea62789d4966097863f13f2146
Daniel Carrasco Baez <daniel.carrasco.baez@accenture.com>
2024-10-23 14:51:20 -0300

    se actualiza infra con ec2 y eip

---
+++ infra-template.tf
@@ -0,0 +1,37 @@
+# Configuración del proveedor de AWS
+provider "aws" {
+  region = var.aws_region  # Se utilizará una variable para la región
+}
+
+resource "aws_instance" "ec2_1" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t3.medium"
+
+  root_block_device {
+    volume_size = 300
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "Instance-t3-medium"
+  }
+}
+
+resource "aws_instance" "ec2_2" {
+  ami           = "ami-0dba2cb6798deb6d8"
+  instance_type = "t3.large"
+
+  root_block_device {
+    volume_size = 5000
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "Instance-t3-large"
+  }
+}
+
+resource "aws_eip" "ec2_1_eip" {
+  instance = aws_instance.ec2_1.id
+}
+
