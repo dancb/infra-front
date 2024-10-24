f1c817bcf10a4ea1209bc6647aebc35659175f4b
Daniel Carrasco Baez <daniel.carrasco.baez@accenture.com>
2024-10-24 17:08:59 -0300

    destroy

--- infra-template.tf
+++ infra-template.tf
@@ -3,31 +3,31 @@ provider "aws" {
   region = var.aws_region  # Se utilizará una variable para la región
 }
 
-resource "aws_instance" "ec2_1" {
-  ami           = "ami-0dba2cb6798deb6d8"
-  instance_type = "t3.medium"
+# resource "aws_instance" "ec2_1" {
+#   ami           = "ami-0dba2cb6798deb6d8"
+#   instance_type = "t3.medium"
 
-  root_block_device {
-    volume_size = 300
-    volume_type = "gp2"
-  }
+#   root_block_device {
+#     volume_size = 300
+#     volume_type = "gp2"
+#   }
 
-  tags = {
-    Name = "Instance-t3-medium"
-  }
-}
+#   tags = {
+#     Name = "Instance-t3-medium"
+#   }
+# }
 
-resource "aws_instance" "ec2_2" {
-  ami           = "ami-0dba2cb6798deb6d8"
-  instance_type = "t3.large"
+# resource "aws_instance" "ec2_2" {
+#   ami           = "ami-0dba2cb6798deb6d8"
+#   instance_type = "t3.large"
 
-  root_block_device {
-    volume_size = 5000
-    volume_type = "gp2"
-  }
+#   root_block_device {
+#     volume_size = 5000
+#     volume_type = "gp2"
+#   }
 
-  tags = {
-    Name = "Instance-t3-large"
-  }
-}
+#   tags = {
+#     Name = "Instance-t3-large"
+#   }
+# }
 
